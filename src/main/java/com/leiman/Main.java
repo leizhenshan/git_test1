885a4395380dd6741da98d79d129db88fd4c27bf
leizhenshan <L17843097694@163.com>
2023-08-19 15:20:26 +0800

    first commit

---
+++ src/main/java/com/leiman/Main.java
@@ -0,0 +1,13 @@
+package com.leiman;
+
+/**
+ * @author lzs
+ * @date ${DATE}
+ * @note
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("git test one");
+        System.out.println("git test two");
+    }
+}
